c9ae4ca0309dff5af8c1ab9011f7be17b52eb23b
ozaki25 <fs65ugumori@yahoo.co.jp>
2017-04-20 15:16:57 +0900

    htmlを変更

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -16,6 +16,7 @@ public class GreetingController {
     @RequestMapping("/greeting")
     public String greeting(@RequestParam(value = "name", required = false, defaultValue = "World")String name, Model model) {
         model.addAttribute("name", name);
+        model.addAttribute("message", "こんにちは");
         return "greeting";
     }
 
